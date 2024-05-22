a91986ab5470854f1ad3e9d6e0e110aaa5be6c26
Vinicius Nogueira <v.francisco@riprisma.com>
2024-05-22 17:22:26 -0300

    alt

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
       currentIndex = (currentIndex + 1) % items.length;
       items[currentIndex].classList.add("active");
     }
-  }, 4000);
+  }, 1114000);
 });
 
 document.addEventListener("DOMContentLoaded", function () {
