caa7522ee07c4fa8887037c4d9bdcb73f0c7db53
Vinicius Nogueira <v.francisco@riprisma.com>
2024-05-22 17:40:05 -0300

    alt

--- js/carrosel.js
+++ js/carrosel.js
@@ -1,111 +1,111 @@
-var swiper = new Swiper(".mySwiper-xp", {
-  loop: false,
-  slidesPerView: "auto",
-  spaceBetween: 12,
+// var swiper = new Swiper(".mySwiper-xp", {
+//   loop: false,
+//   slidesPerView: "auto",
+//   spaceBetween: 12,
 
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
 
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
 
-var swiper = new Swiper(".mySwiper-invest", {
-  loop: false,
-  slidesPerView: "auto",
-  spaceBetween: 16,
+// var swiper = new Swiper(".mySwiper-invest", {
+//   loop: false,
+//   slidesPerView: "auto",
+//   spaceBetween: 16,
 
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
 
-  pagination: {
-    el: ".swiper-pagination",
-  },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
 
-var swiper = new Swiper(".mySwiper-cards-home", {
-  loop: false,
-  slidesPerView: "auto",
-  spaceBetween: 12,
+// var swiper = new Swiper(".mySwiper-cards-home", {
+//   loop: false,
+//   slidesPerView: "auto",
+//   spaceBetween: 12,
 
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
 
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
 
-function initSwiper() {
-  console.log("Width: ", window.innerWidth);
-  if (window.innerWidth < 992) {
-    console.log("Initializing Swiper");
-    var swiper = new Swiper(".swiper-cards-invest", {
-      loop: false,
-      slidesPerView: "auto",
-      spaceBetween: 16,
+// function initSwiper() {
+//   console.log("Width: ", window.innerWidth);
+//   if (window.innerWidth < 992) {
+//     console.log("Initializing Swiper");
+//     var swiper = new Swiper(".swiper-cards-invest", {
+//       loop: false,
+//       slidesPerView: "auto",
+//       spaceBetween: 16,
 
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
+//       autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//       },
 
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-  }
-}
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+//     });
+//   }
+// }
 
-initSwiper();
+// initSwiper();
 
-window.addEventListener("resize", function () {
-  console.log("Window resized");
-  initSwiper();
-});
+// window.addEventListener("resize", function () {
+//   console.log("Window resized");
+//   initSwiper();
+// });
 
-///////////////////
+// ///////////////////
 
-function initSwiperPerson() {
-  console.log("Width: ", window.innerWidth);
-  if (window.innerWidth < 992) {
-    console.log("Initializing Swiper");
-    var swiper = new Swiper(".swiper-person", {
-      loop: false,
-      spaceBetween: 24,
-      slidesPerView: "auto",
+// function initSwiperPerson() {
+//   console.log("Width: ", window.innerWidth);
+//   if (window.innerWidth < 992) {
+//     console.log("Initializing Swiper");
+//     var swiper = new Swiper(".swiper-person", {
+//       loop: false,
+//       spaceBetween: 24,
+//       slidesPerView: "auto",
 
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
+//       autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//       },
 
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-  }
-}
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+//     });
+//   }
+// }
 
-initSwiperPerson();
+// initSwiperPerson();
 
-window.addEventListener("resize", function () {
-  console.log("Window resized");
-  initSwiperPerson();
-});
+// window.addEventListener("resize", function () {
+//   console.log("Window resized");
+//   initSwiperPerson();
+// });
